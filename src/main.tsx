cc683f67c304c0cfc9bb17320a78eddfc7708eda
Carles Pueyo <cpcastells@gmail.com>
2023-05-29 22:22:33 +0200

    Create GlobalStyle and add to main

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,17 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme/theme";
+import { GlobalStyle } from "./styles/GlobalStyle/GlobalStyle";
+import "@fontsource/jost";
+import "@fontsource/poppins";
+import "@fontsource/nunito";
+import "@fontsource/luckiest-guy";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
+        <GlobalStyle />
         <App />
       </Provider>
     </ThemeProvider>
