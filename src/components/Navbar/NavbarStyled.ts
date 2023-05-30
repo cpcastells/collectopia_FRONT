b5849d8c4e84b5aa8cb827253befdc59a646cb08
Carles Pueyo <cpcastells@gmail.com>
2023-05-30 10:14:44 +0200

    Add NavbarStyled

---
+++ src/components/Navbar/NavbarStyled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const NavbarStyled = styled.nav`
+  .navbar-links {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+`;
+
+export default NavbarStyled;
