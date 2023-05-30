9b91955fcddb29f4ef39a71317c6e8854854b0e7
Carles Pueyo <cpcastells@gmail.com>
2023-05-30 15:00:49 +0200

    Add LoginPage route in appRoute

--- src/routers/appRouter/appRouter.tsx
+++ src/routers/appRouter/appRouter.tsx
@@ -1,10 +1,12 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../../components/App/App";
+import LoginPage from "../../pages/login/LoginPage";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
+    children: [{ path: "/login", element: <LoginPage /> }],
   },
 ];
 
