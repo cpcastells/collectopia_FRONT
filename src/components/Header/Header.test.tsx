82f5fce5051edd505014d20e030f3b3cf2a59520
Carles Pueyo <cpcastells@gmail.com>
2023-05-30 12:37:43 +0200

    Create Header test

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,20 @@
+import { screen } from "@testing-library/react";
+import renderWithProviders from "../../utils/testUtils";
+import Header from "./Header";
+
+describe("Given a Header component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a heading with the title 'COLLECTOPIA'", () => {
+      const expectedTitle = "COLLECTOPIA";
+
+      renderWithProviders(<Header />);
+
+      const header = screen.getByRole("heading", {
+        level: 1,
+        name: expectedTitle,
+      });
+
+      expect(header).toBeInTheDocument();
+    });
+  });
+});
