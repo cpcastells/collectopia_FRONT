1c1f0a4a90dae28da5af0db24578149d7c3d7594
Carles Pueyo <cpcastells@gmail.com>
2023-05-30 23:44:21 +0200

    Refactor Navbar tests

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -4,34 +4,36 @@ import renderWithProviders from "../../utils/testUtils";
 
 describe("Given a Navbar component", () => {
   describe("When it renders", () => {
-    test("Then it should show a logout icon with alt property 'logout user'", () => {
-      const expectedAltText = "logout user";
+    test("Then it should show a button with an icon of logout", () => {
+      const altTextLogout = "logout user";
 
       renderWithProviders(<Navbar />);
 
-      const navbar = screen.getByAltText(expectedAltText);
+      const logoutLink = screen.getByAltText(altTextLogout);
 
-      expect(navbar).toBeInTheDocument();
+      expect(logoutLink).toBeInTheDocument();
     });
   });
 
-  test("Then it should show a home icon with alt property 'collection boardgames'", () => {
-    const expectedAltText = "collection boardgames";
+  test("Then it should show a home link", () => {
+    const ariaTextCollection = "boardgame collection";
 
     renderWithProviders(<Navbar />);
 
-    const navbar = screen.getByAltText(expectedAltText);
+    const homeLink = screen.getByRole("link", { name: ariaTextCollection });
 
-    expect(navbar).toBeInTheDocument();
+    expect(homeLink).toBeInTheDocument();
   });
 
-  test("Then it should show a add icon with alt property 'add boardgame'", () => {
-    const expectedAltText = "add boardgame";
+  test("Then it should show a add boardgame link", () => {
+    const addBoardgameText = "add boardgame";
 
     renderWithProviders(<Navbar />);
 
-    const navbar = screen.getByAltText(expectedAltText);
+    const addBoardgameLink = screen.getByRole("link", {
+      name: addBoardgameText,
+    });
 
-    expect(navbar).toBeInTheDocument();
+    expect(addBoardgameLink).toBeInTheDocument();
   });
 });
