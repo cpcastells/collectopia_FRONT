ee3a141fbe521fef633d867e1216ec8202d21ed7
Carles Pueyo <cpcastells@gmail.com>
2023-05-30 22:59:48 +0200

    Refactor classNames to BEM

--- src/components/LoginForm/LoginFormStyled.tsx
+++ src/components/LoginForm/LoginFormStyled.tsx
@@ -9,34 +9,36 @@ const LoginFormStyled = styled.form`
   border-radius: 31px;
   padding: 50px 42px 20px;
 
-  .login-field {
-    font-family: ${(props) => props.theme.fonts.form};
-    color: ${(props) => props.theme.colors["dark-green"]};
-    font-size: 1rem;
-    background: none;
-    border: none;
-    border-bottom: 1.3px solid ${(props) => props.theme.colors["dark-green"]};
-    width: 100%;
-
-    &::placeholder {
+  .login-form {
+    &__field {
+      font-family: ${(props) => props.theme.fonts.form};
       color: ${(props) => props.theme.colors["dark-green"]};
+      font-size: 1rem;
+      background: none;
+      border: none;
+      border-bottom: 1.3px solid ${(props) => props.theme.colors["dark-green"]};
+      width: 100%;
+
+      &::placeholder {
+        color: ${(props) => props.theme.colors["dark-green"]};
+      }
+
+      &:focus {
+        border-bottom: 2px solid ${(props) => props.theme.colors["dark-green"]};
+        outline: none;
+      }
     }
 
-    &:focus {
-      border-bottom: 2px solid ${(props) => props.theme.colors["dark-green"]};
-      outline: none;
+    &__button {
+      background: ${(props) => props.theme.colors["dark-green"]};
+      color: ${(props) => props.theme.colors.background};
+      font-family: ${(props) => props.theme.fonts.form};
+      font-size: 1.125rem;
+      font-weight: 700;
+      border-radius: 15px;
+      width: 196px;
+      height: 51px;
     }
-  }
-
-  .login-button {
-    background: ${(props) => props.theme.colors["dark-green"]};
-    color: ${(props) => props.theme.colors.background};
-    font-family: ${(props) => props.theme.fonts.form};
-    font-size: 1.125rem;
-    font-weight: 700;
-    border-radius: 15px;
-    width: 196px;
-    height: 51px;
   }
 `;
 
