8b42a47f033c5b038fb26df4b0604bf57620c8eb
Carles Pueyo <cpcastells@gmail.com>
2023-05-31 13:14:32 +0200

    Create getLocalStorageItem

--- src/hooks/useLocalStorage/useLocalStorage.ts
+++ src/hooks/useLocalStorage/useLocalStorage.ts
@@ -2,11 +2,16 @@ const useLocalStorage = () => {
   const setLocalStorageItem = (
     localStorageKey: string,
     localStorasgeItem: string
-  ) => {
+  ): void => {
     localStorage.setItem(localStorageKey, localStorasgeItem);
   };
 
-  return { setLocalStorageItem };
+  const getLocalStorageItem = (localStorageKey: string): string | null => {
+    const token = localStorage.getItem(localStorageKey);
+    return token;
+  };
+
+  return { setLocalStorageItem, getLocalStorageItem };
 };
 
 export default useLocalStorage;
