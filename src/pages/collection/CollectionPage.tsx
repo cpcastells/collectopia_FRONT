cb96d6c05ef4ce515f315b29200883db94f9e7c3
Carles Pueyo <cpcastells@gmail.com>
2023-06-01 12:34:37 +0200

    Create CollectionPage page

---
+++ src/pages/collection/CollectionPage.tsx
@@ -0,0 +1,13 @@
+import CollectionPageStyled from "./CollectionPageStyled";
+
+const Collection = (): React.ReactElement => {
+  return (
+    <>
+      <CollectionPageStyled>
+        <h2 className="collection-title">My collection</h2>
+      </CollectionPageStyled>
+    </>
+  );
+};
+
+export default Collection;
