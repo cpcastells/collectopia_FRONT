a5f0eb70e843b281783e0c28b8e79cbaf195bbe5
Carles Pueyo <cpcastells@gmail.com>
2023-06-02 17:45:22 +0200

    Add boardgames dispatch to CollectionPage

--- src/pages/collection/CollectionPage.tsx
+++ src/pages/collection/CollectionPage.tsx
@@ -1,10 +1,22 @@
-import CollectionPageStyled from "./CollectionPageStyled";
+import { useEffect } from "react";
+import { useAppDispatch } from "../../store";
+import CollectionPageStyled from "./CollectionPageStyled.js";
+import { loadBoardgamesActionCreator } from "../../store/boardgames/boardgameSlice.js";
+import { boardGamesMock } from "../../mocks/boardgames/boardgamesMocks.js";
+import BoardgamesList from "../../components/CollectionList/BoardgamesList.js";
 
 const CollectionPage = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(loadBoardgamesActionCreator(boardGamesMock));
+  });
+
   return (
     <>
       <CollectionPageStyled>
         <h2 className="collection-title">My collection</h2>
+        <BoardgamesList />
       </CollectionPageStyled>
     </>
   );
