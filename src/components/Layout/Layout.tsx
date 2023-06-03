3e6992de57cb56628f4c87728b30bec26e41fb00
Carles Pueyo <cpcastells@gmail.com>
2023-06-04 01:20:05 +0200

    Add Loader rendering to Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,13 +2,17 @@ import ContainerStyled from "../shared/ContainerStyled";
 import Header from "../Header/Header";
 import { Outlet, useLocation } from "react-router-dom";
 import paths from "../../routers/paths";
+import { useAppSelector } from "../../store";
+import Loader from "../Loader/Loader";
 
 const Layout = (): React.ReactElement => {
   const location = useLocation();
+  const { isLoading } = useAppSelector((state) => state.uiStore);
 
   return (
     <>
       <ContainerStyled>
+        {isLoading && <Loader />}
         {location.pathname !== `${paths.login}` && <Header />}
         <Outlet />
       </ContainerStyled>
