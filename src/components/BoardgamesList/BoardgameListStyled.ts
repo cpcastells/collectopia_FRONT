a1fa2db3d24e2578841f68370e29e6e5944797b2
Carles Pueyo <cpcastells@gmail.com>
2023-06-03 13:55:51 +0200

    Modify paddings to CollectionPage title

---
+++ src/components/BoardgamesList/BoardgameListStyled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const BoardgamesListStyled = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 26px;
+`;
+
+export default BoardgamesListStyled;
