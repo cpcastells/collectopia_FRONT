2889235d6b027f34a407e813a793bd6ab7a23c3c
Carles Pueyo <cpcastells@gmail.com>
2023-06-03 19:21:40 +0200

    Create Loader component

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -34,19 +34,17 @@ const LoginPage = (): React.ReactElement => {
   };
 
   return (
-    <>
-      <LoginPageStyled>
-        <img
-          src="/images/main-logo.svg"
-          alt="collectopia logo"
-          width={220}
-          height={120}
-          className="main-logo"
-        />
-        <h2>Track your Boardgames here!</h2>
-        <LoginForm submitForm={submitForm} />
-      </LoginPageStyled>
-    </>
+    <LoginPageStyled>
+      <img
+        src="/images/main-logo.svg"
+        alt="collectopia logo"
+        width={220}
+        height={120}
+        className="main-logo"
+      />
+      <h2>Track your Boardgames here!</h2>
+      <LoginForm submitForm={submitForm} />
+    </LoginPageStyled>
   );
 };
 
