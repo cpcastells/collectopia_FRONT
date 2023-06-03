4b8451977759fc6a0886b177e84cb459b7603a39
Carles Pueyo <cpcastells@gmail.com>
2023-06-04 00:56:37 +0200

    Add show and hideLoading in getBoardgames function of useBoardgames hook

--- src/hooks/useBoardgames/useBoardgames.ts
+++ src/hooks/useBoardgames/useBoardgames.ts
@@ -5,21 +5,31 @@ import {
   BoardgameStructure,
   BoardgamesApiResponse,
 } from "../../store/boardgames/types";
-import { useAppSelector } from "../../store";
+import { useAppDispatch, useAppSelector } from "../../store";
+import {
+  hideLoadingActionCreator,
+  showLoadingActionCreator,
+} from "../../store/ui/uiSlice";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const useBoardgames = () => {
   const { token } = useAppSelector((state) => state.userStore);
+  const dispatch = useAppDispatch();
 
   const getBoardgames = useCallback(async (): Promise<BoardgameStructure[]> => {
+    dispatch(showLoadingActionCreator());
+
     const {
       data: { boardgames },
     } = await axios.get<BoardgamesApiResponse>(`${apiUrl}${paths.boardgames}`, {
       headers: { Authorization: `Bearer ${token}` },
     });
+
+    dispatch(hideLoadingActionCreator());
+
     return boardgames;
-  }, [token]);
+  }, [dispatch, token]);
   return { getBoardgames };
 };
 
