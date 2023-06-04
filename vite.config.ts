88a6219122d2aec050cc1f14318178a675b3f500
Carles Pueyo <cpcastells@gmail.com>
2023-06-04 19:24:47 +0200

    Configure vite-plugin-svgr plugin and move icons to assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
+import svgr from "vite-plugin-svgr";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   test: {
     globals: true,
     environment: "jsdom",
