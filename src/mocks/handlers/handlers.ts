3b579f7d3f17a266faf12bb693819e86427d0e98
Carles Pueyo <cpcastells@gmail.com>
2023-06-05 17:17:17 +0200

    Add error handlers for GET /Boardgames

--- src/mocks/handlers/handlers.ts
+++ src/mocks/handlers/handlers.ts
@@ -24,3 +24,9 @@ export const handlers = [
     );
   }),
 ];
+
+export const errorHandlers = [
+  rest.get(`${apiUrl}${paths.boardgames}`, (_req, res, ctx) => {
+    return res(ctx.status(500), ctx.json({ message: "Error with the server" }));
+  }),
+];
