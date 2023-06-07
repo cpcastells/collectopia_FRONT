db4df2e37331ef03a1568fdfc2edae0d6e175ab0
Carles Pueyo <cpcastells@gmail.com>
2023-06-07 13:12:21 +0200

    Create AddBoardgamePage

---
+++ src/pages/AddBoardgamePage/AddBoardgamePage.tsx
@@ -0,0 +1,12 @@
+import AddBoardgamePageStyled from "./AddBoardgamePageStyled";
+
+const AddBoardgamePage = (): React.ReactElement => {
+  return (
+    <AddBoardgamePageStyled>
+      <h2 className="add-title">add</h2>
+      <span className="add-subtitle">bordgame!</span>
+    </AddBoardgamePageStyled>
+  );
+};
+
+export default AddBoardgamePage;
