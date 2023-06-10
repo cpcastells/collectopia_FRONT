6e98f3de168394f3437ccda3e5abfa1966d62da1
Carles Pueyo <cpcastells@gmail.com>
2023-06-10 17:30:53 +0200

    Add click use-case test to Pagination component tests

--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -1,17 +1,35 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { renderWithProviders } from "../../utils/testUtils";
 import Pagination from "./Pagination";
+import { vi } from "vitest";
 
-describe("Given a Loader component", () => {
+describe("Given a pagination component", () => {
+  const handleOnClick = vi.fn();
+
   describe("When it is rendered", () => {
-    test("Then it should show a loading animation", () => {
+    test("Then it should show a load more button", () => {
       const expectedAltText = "load more button";
 
-      renderWithProviders(<Pagination />);
+      renderWithProviders(<Pagination onClick={handleOnClick} />);
 
-      const image = screen.getByAltText(expectedAltText);
+      const button = screen.getByAltText(expectedAltText);
 
-      expect(image).toBeInTheDocument();
+      expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe("When it receives a function and the user clicks on load more button", () => {
+    test("Then it should show call that function", async () => {
+      const expectedAltText = "load more button";
+
+      renderWithProviders(<Pagination onClick={handleOnClick} />);
+
+      const button = screen.getByAltText(expectedAltText);
+
+      await userEvent.click(button);
+
+      expect(handleOnClick).toHaveBeenCalled();
     });
   });
 });
