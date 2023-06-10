655b62e48fa65f70f76382fa98aace34c8aa26e0
Carles Pueyo <cpcastells@gmail.com>
2023-06-10 17:33:16 +0200

    Add load more click use-case test to CollectionPage tests

--- src/pages/CollectionPage/CollectionPage.test.tsx
+++ src/pages/CollectionPage/CollectionPage.test.tsx
@@ -1,6 +1,9 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { renderWithProviders } from "../../utils/testUtils";
 import CollectionPage from "./CollectionPage";
+
+import { boardGamesMock } from "../../mocks/boardgames/boardgamesMocks";
 
 describe("Given a CollectionPage page", () => {
   describe("When it is rendered", () => {
@@ -13,6 +16,21 @@ describe("Given a CollectionPage page", () => {
         level: 2,
         name: expectedTest,
       });
+
+      expect(heading).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user clicks on the button 'Load More'", () => {
+    test("Then it shoud render the a new stack of boardgames", async () => {
+      renderWithProviders(<CollectionPage />, {
+        boardgameStore: { boardgames: boardGamesMock, stack: 5 },
+      });
+
+      const heading = screen.getByRole("heading", { name: "Rising Sun" });
+      const button = screen.getByAltText("load more button");
+
+      await userEvent.click(button);
 
       expect(heading).toBeInTheDocument();
     });
